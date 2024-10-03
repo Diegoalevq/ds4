a4be88dcd3e3c48c377a8e18f2a29564152bcd40
Diegoalevq <diegoalevq@gmail.com>
2024-10-02 21:27:34 -0500

    Laboratorio 11 completado.

---
+++ Parcial 1/Parcial 1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+
+public class Program
+
+{
+      public static void Main(string[] args)
+    {
+        
+        int n;
+        do
+        {
+            Console.Write("Introduce el tamaño de la matriz (Solo numero impar): ");
+            n = int.Parse(Console.ReadLine());
+        } while (n % 2 == 0); 
+
+       
+        int[,] matriz = new int[n, n];
+        Random random = new Random();
+
+        
+        for (int i = 0; i < n; i++)
+        {
+            matriz[i, n - 1 - i] = random.Next(101, 201); 
+        }
+
+       
+        Console.WriteLine("Matriz generada:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(matriz[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+       
+        int sumaDiagonalPrincipal = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sumaDiagonalPrincipal += matriz[i, i];
+        }
+
+       
+        int sumaDiagonalInversa = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sumaDiagonalInversa += matriz[i, n - 1 - i];
+        }
+
+       
+        Console.WriteLine($"Suma de la diagonal principal: {sumaDiagonalPrincipal}");
+        Console.WriteLine($"Suma de la diagonal inversa: {sumaDiagonalInversa}");
+    }
+}
